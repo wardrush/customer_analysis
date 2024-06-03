a009cc6d0ae9af3b1c6c0ecf88fca1e8a3212b26
wardrush <wardrushton@gmail.com>
2024-06-03 16:40:01 -0500

    Modularize Main Render Logic
    -Package Main Streamlit app into functions for easy reoder
    -Update initial analysis file

--- main.py
+++ main.py
@@ -20,21 +20,24 @@ def manage_file_uploader():
                 # Show a file preview
                 st.write("File Preview:")
                 st.dataframe(df.head())
+                return uploaded_file
         except pd.errors.EmptyDataError:
             st.error("No columns to parse from file. Please upload a valid CSV file.")
         except Exception as e:
             st.error(f"An error occurred: {e}")
 
-def manage_criteria_selection():
-    # Text input for criteria
-    criteria = st.text_input("Enter criteria for complete records (comma-separated)", "email,phone_number")
+def process_criteria_selection(criteria_input=None):
+    criteria_list = [c.strip() for c in criteria_input.split(',')]
+    return criteria_list
 
-def manage_initial_estimate_display():
+def manage_initial_estimate_display(initial_estimate_field=None, complete_percentage=0.00):
     # Initial estimate title line
-    initial_estimate = st.empty()
-    initial_estimate.header("Initial Completeness Estimate: 0%")
+    if initial_estimate_field == None:
+        initial_estimate_field = st.empty()
+    initial_estimate_field.write(f"Record Completeness Estimate: {complete_percentage:.2f}%")
+    return initial_estimate_field
 
-def manage_analysis_button():
+def manage_analysis_button(uploaded_file=None, criteria_list=None, initial_estimate_field=None):
     # Run analysis button
     if st.button("Run Analysis"):
         if uploaded_file is not None:
@@ -43,8 +46,6 @@ def manage_analysis_button():
                 st.dataframe(uploaded_file)
                 customers = read_csv(io.StringIO(uploaded_file.getvalue().decode('utf-8')))
 
-                # Split the criteria input into a list
-                criteria_list = [c.strip() for c in criteria.split(',')]
                 # Analyze the customer data based on criteria
                 complete_records, incomplete_records = analyze_customers(customers, criteria_list)
 
@@ -52,12 +53,10 @@ def manage_analysis_button():
                 total_records = len(customers)
                 complete_percentage = (len(complete_records) / total_records) * 100 if total_records > 0 else 0
 
-                # Update the initial estimate title line
-                initial_estimate.text(f"Initial Estimate of Complete Records: {complete_percentage:.2f}%")
 
                 # Display the analysis results
-                st.write(f"Complete Records: {len(complete_records)}")
-                st.write(f"Incomplete Records: {len(incomplete_records)}")
+                #st.write(f"Complete Records: {len(complete_records)}")
+                #st.write(f"Incomplete Records: {len(incomplete_records)}")
 
                 # Offer Email Generation
                 # user_email = st.text_input("Enter your email to receive the report")
@@ -70,23 +69,35 @@ def manage_analysis_button():
             st.error("Please upload a CSV file to run the analysis.")
 
 def manage_email_capture():
+    # Capture email address from text field
     pass
 
 def manage_report_send():
     pass
 
 def main():
+    # Set Analyis Flag
+    is_analyzed = False
+
     # Set the title of the app
     st.title("Customer Data Analysis")
-    manage_initial_estimate_display()
-    st.header('1. Upload File', divider='blue', help='Upload a csv file with customer data here')
-    manage_file_uploader()
-    st.header('2. Select Criteria', divider='blue', help='Choose what completeness consists of')
-    manage_criteria_selection()
-    manage_analysis_button()
-    manage_initial_estimate_display()
+    initial_estimate_field = manage_initial_estimate_display()
 
-    st.header('3. Create and Send Report', divider='blue')
+    # Section 1: File Upload
+    st.header('1. Upload File', help='Upload a csv file with customer data here')
+    uploaded_file = manage_file_uploader()
+
+    # Section 2: Criteria Selection
+    st.header('2. Select Criteria', help='Choose what completeness consists of')
+    criteria_list_placeholder = st.empty()
+    criteria = criteria_list_placeholder.text_input("Enter criteria for complete records (comma-separated)",
+                                                    "email,phone_number")
+    criteria_list = process_criteria_selection(criteria)
+    manage_analysis_button(uploaded_file=uploaded_file, criteria_list=criteria_list)
+    manage_initial_estimate_display(initial_estimate_field=initial_estimate_field)
+
+    # Section 3 Report Management
+    st.header('3. Create and Send Report')
     manage_email_capture()
     manage_report_send()
 
