1a396a734925944479ed4bbc596794131c98539e
wardrush <wardrushton@gmail.com>
2024-06-16 10:47:10 -0400

    Clean Up Boilerplate Text and Edit Sample data

--- app/data/boilerplate.py
+++ app/data/boilerplate.py
@@ -1,12 +1,52 @@
 #app/report/boilerplate.py
-# This file holds the boilerplate strings for the report
+# This file holds the boilerplate strings
+
+headers_how_to_use_this_tool ="""
+                    **Purpose:**
+
+                    The Completeness Check Tool helps you evaluate the quality of your customer data by analyzing how many records meet specific completeness criteria. This can be invaluable during due diligence or data quality assessments.
+
+                    **How It Works:**
+
+                    Upload your customer CRM dataset and specify the columns that are required for a record to be considered complete (e.g., email and phone).
+                        """
+
+
+headers_how_to_use_this_tool_info="""
+                    **Expected Outputs:**
+
+                    1. **Percentage of Complete Records:** The tool calculates the percentage of records that have non-null values for all specified criteria.
+                    2. **Summary Table:** The table provides a detailed breakdown of:
+                       - Percentage and number of records with non-null values for each individual criterion.
+                       - Overall percentage and number of records that meet all the specified criteria.
+                """
+
+headers_example_use_cases_and_tips="""
+                    **Example Use Case:**
+
+                    Imagine you need to assess the quality of your contact data before merging databases. You want to ensure that each contact has both an email and a phone number. Here's how you would use this tool: Upload your dataset, define your criteria, and run the tool to get the results.
+
+                    **Why Use This Tool?**
+
+                    - **Quick Assessment:** Instantly see the quality of your data based on your defined criteria.
+                    - **Informed Decisions:** Use the summary table to understand where your data might be lacking and make informed decisions on data cleaning or enhancement.
+                    - **Flexible and Customizable:** Define any number of criteria to suit your specific needs.
+                    """
+
+headers_example_use_cases_and_tips_info="""
+                **Tips:**
+
+                - **Regular Usage:** Keep this tool handy during data migration, integration projects, or regular data quality checks.
+                - **Feedback Welcome:** If you have suggestions for improving this tool, please reach out directly or open an issue on our GitHub page. Your feedback helps keep this tool accurate and useful.
+                """
+
 
 company_overview = """Customer Health Check is dedicated to providing Private Equity firms with precise and actionable insights into the health of customer databases. Our proprietary AI-enabled tool streamlines the due diligence process by identifying the completeness of customer records, ensuring that you have a clear understanding of the customer base before making critical acquisition decisions.
-
-Our mission is to empower your due diligence efforts with reliable data analysis, reducing the risk of post-acquisition surprises and facilitating informed decision-making.
-
-For further information or inquiries, please contact us:
-"""
+                
+                Our mission is to empower your due diligence efforts with reliable data analysis, reducing the risk of post-acquisition surprises and facilitating informed decision-making.
+                
+                For further information or inquiries, please contact us:
+                """
 
 contact_table_data = [
         ['Company Name', 'Customer Health Check'],
@@ -25,15 +65,15 @@ tool_methodology_and_limitations="""The Customer Health Check tool provides a co
                                     """
 
 common_next_steps="""
-## Common Next Steps
-
-The output of the Customer Health Check tool represents the first step in understanding the health of the customer database. This initial analysis highlights the completeness of records and identifies potential gaps that need further investigation.
-
-Typically, the next steps involve a manual discovery period conducted by a consultancy to validate and verify the findings. This phase includes in-depth analysis and validation to ensure the accuracy of the customer data and to address any identified issues.
-
-While we offer consultancy services to assist with this manual discovery period, it is not a requirement. Your team can choose to engage our consultancy services for a seamless transition from analysis to action, or you may opt to utilize your internal resources or third-party consultants to complete the due diligence process.
-
-Our goal is to provide you with the tools and insights needed to make informed decisions, ensuring a smooth and successful acquisition process.
-"""
+                ## Common Next Steps
+                
+                The output of the Customer Health Check tool represents the first step in understanding the health of the customer database. This initial analysis highlights the completeness of records and identifies potential gaps that need further investigation.
+                
+                Typically, the next steps involve a manual discovery period conducted by a consultancy to validate and verify the findings. This phase includes in-depth analysis and validation to ensure the accuracy of the customer data and to address any identified issues.
+                
+                While we offer consultancy services to assist with this manual discovery period, it is not a requirement. Your team can choose to engage our consultancy services for a seamless transition from analysis to action, or you may opt to utilize your internal resources or third-party consultants to complete the due diligence process.
+                
+                Our goal is to provide you with the tools and insights needed to make informed decisions, ensuring a smooth and successful acquisition process.
+                """
 
 
--- app/utils.py
+++ app/utils.py
@@ -6,44 +6,15 @@ import os
 from pathlib import Path
 import io
 import streamlit as st
+from .data import boilerplate
 
 def manage_info_blurb():
     with st.expander("**📖 How to Use This Tool**"):
-        st.markdown("""
-                    **Purpose:**
-
-                    The Completeness Check Tool helps you evaluate the quality of your customer data by analyzing how many records meet specific completeness criteria. This can be invaluable during due diligence or data quality assessments.
-
-                    **How It Works:**
-
-                    Upload your customer CRM dataset and specify the columns that are required for a record to be considered complete (e.g., email and phone).
-        """)
-        st.info("""
-                    **Expected Outputs:**
-
-                    1. **Percentage of Complete Records:** The tool calculates the percentage of records that have non-null values for all specified criteria.
-                    2. **Summary Table:** The table provides a detailed breakdown of:
-                       - Percentage and number of records with non-null values for each individual criterion.
-                       - Overall percentage and number of records that meet all the specified criteria.
-                """)
+        st.markdown(boilerplate.headers_how_to_use_this_tool)
+        st.info(boilerplate.headers_how_to_use_this_tool_info)
     with st.expander("**🔧 Example Use Cases & Tips**"):
-        st.markdown("""
-                    **Example Use Case:**
-
-                    Imagine you need to assess the quality of your contact data before merging databases. You want to ensure that each contact has both an email and a phone number. Here's how you would use this tool: Upload your dataset, define your criteria, and run the tool to get the results.
-
-                    **Why Use This Tool?**
-
-                    - **Quick Assessment:** Instantly see the quality of your data based on your defined criteria.
-                    - **Informed Decisions:** Use the summary table to understand where your data might be lacking and make informed decisions on data cleaning or enhancement.
-                    - **Flexible and Customizable:** Define any number of criteria to suit your specific needs.
-                    """)
-        st.info("""
-                **Tips:**
-
-                - **Regular Usage:** Keep this tool handy during data migration, integration projects, or regular data quality checks.
-                - **Feedback Welcome:** If you have suggestions for improving this tool, please reach out directly or open an issue on our GitHub page. Your feedback helps keep this tool accurate and useful.
-                """)
+        st.markdown(boilerplate.headers_example_use_cases_and_tips)
+        st.info(boilerplate.headers_example_use_cases_and_tips_info)
 
 def manage_file_uploader():
     # Radio button to select between uploading a file and using test data
