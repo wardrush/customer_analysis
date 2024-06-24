726a360dc1ad6460e1cff902b1d7897ff8d7ce48
wardrush <wardrushton@gmail.com>
2024-06-23 21:24:43 -0500

    Redeploy for Heroku

--- app/main_test.py
+++ app/main_test.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+# Setting the page configuration to use the wide layout
+st.set_page_config(layout="wide")
+
+# Main content
 st.title("Test App Runs!")
+
+# Ensure the sidebar is empty
+with st.sidebar:
+    st.empty()
